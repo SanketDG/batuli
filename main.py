d600677c1cd2efc3fead9f27efede34168ae6820
trishnaguha <trishnaguha17@gmail.com>
2015-08-07 00:27:47 +0530

    Adding some features

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from twisted.internet import reactor, protocol
 from twisted.words.protocols import irc
-
+import time
 
 class LoggingIRCClient(irc.IRCClient):
 
@@ -13,8 +13,14 @@ class LoggingIRCClient(irc.IRCClient):
         user = user.split('!')[0]
         if msg == '~pym':
             self.msg(channel, 'http://pymbook.readthedocs.org/en/latest/')
-        if msg.strip() == u'hello ' + self.nickname:
-            self.msg(channel, '{} hello!'.format(user))
+        if msg == self.nickname + ',' + ' hello':
+            self.msg(channel, '{}, hello!'.format(user))
+        if msg == self.nickname + ',' + ' ping':
+            self.msg(channel, '{}, pong'.format(user))
+        if msg == self.nickname + ',' + ' quit':
+            self.quit('')
+        if msg == self.nickname + ',' + ' date' or msg == 'date':
+            self.msg(channel, "Current date & time " + time.strftime("%c"))
 
 
 def main():
